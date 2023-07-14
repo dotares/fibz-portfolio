2b0a4897653d81f551eb95a13eb1b912cba8bc9d
fibbbonachos <arish2k3@gmail.com>
2023-07-14 17:46:00 +0530

    write navbar component init

--- src/frontend/src/App.jsx
+++ src/frontend/src/App.jsx
@@ -1,7 +1,7 @@
-import { Navbar } from "./components/Navbar.jsx";
-import { Hero } from "./components/Hero.jsx";
-import { About } from "./components/About.jsx";
-import { Projects } from "./components/Projects.jsx";
+import Navbar from "./components/Navbar.jsx";
+import Hero from "./components/Hero.jsx";
+import About from "./components/About.jsx";
+import Projects from "./components/Projects.jsx";
 
 function App() {
   return (
--- src/frontend/src/components/Navbar.jsx
+++ src/frontend/src/components/Navbar.jsx
@@ -1,3 +1,15 @@
 export default function Navbar() {
-  return <>Navbar</>;
+  return (
+    <div className="navbar-section">
+      <div className="home-link">
+        <a href="#">brand</a>
+      </div>
+      <div className="about-link">
+        <a href="#about">about</a>
+      </div>
+      <div className="projects-link">
+        <a href="#projects">projects</a>
+      </div>
+    </div>
+  );
 }
