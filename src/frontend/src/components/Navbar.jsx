23fbdb3426b432a76217e49e02db3295b527f99d
fibbbonachos <arish2k3@gmail.com>
2023-07-14 22:18:35 +0530

    add notes for later development sessions

--- src/frontend/src/components/Navbar.jsx
+++ src/frontend/src/components/Navbar.jsx
@@ -1,15 +1,26 @@
 export default function Navbar() {
+  // NOTE: added text white only for testing purposes
+  // the flex is supposed to be intentional
+
+  // TODO: Fix the tailwind config content
+
   return (
-    <div className="navbar-section">
-      <a className="brand-logo" href="#">
-        brand
-      </a>
-      <a className="about-link" href="#about">
-        about
-      </a>
-      <a className="projects-link" href="#projects">
-        projects
-      </a>
+    <div className="navbar-section flex">
+      <div>
+        <a className="brand-logo text-white" href="#">
+          brand
+        </a>
+      </div>
+      <div>
+        <a className="about-link" href="#about">
+          about
+        </a>
+      </div>
+      <div>
+        <a className="projects-link" href="#projects">
+          projects
+        </a>
+      </div>
     </div>
   );
 }
