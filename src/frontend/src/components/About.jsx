3a4960d5367142b83656437b2349cf1ff2df3c9a
fibbbonachos <arish2k3@gmail.com>
2023-07-14 18:54:06 +0530

    add hero section content and also about me section content

--- src/frontend/src/components/About.jsx
+++ src/frontend/src/components/About.jsx
@@ -1,3 +1,15 @@
 export default function About() {
-  return <div className="about-section">about section</div>;
+  return (
+    <div className="about-section">
+      <h2 className="secondary-header">A little about me</h2>
+      <p className="paragraph-content">
+        A full-stack developer with a passion for building beautiful and
+        functional web applications. Proficient in the MERN stack, handling both
+        the front- and back-end of a project. I'm a highly motivated and
+        results-oriented individual with a proven track record of success who's
+        always willing to go the extra mile. Excited to use my skills and
+        experience to help you build the next great web application.
+      </p>
+    </div>
+  );
 }
--- src/frontend/src/components/Hero.jsx
+++ src/frontend/src/components/Hero.jsx
@@ -1,3 +1,15 @@
+import heroImage from "../assets/Fibbonachos-HERO.png";
+
 export default function Hero() {
-  return <div className="hero-section">hero section</div>;
+  return (
+    <div className="hero-section">
+      <h1 className="primary-header">
+        Hi, I'm Fibz aka Arish, and I'm a REAL full-stack developer.
+      </h1>
+      <h3 className="tertiary-header">
+        Let's cut the crap and build something great together
+      </h3>
+      <img src={heroImage} alt="Hero Image" />
+    </div>
+  );
 }
