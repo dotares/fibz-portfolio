939c390a7078d8505a942f8d864e998e8eb39589
fibbbonachos <arish2k3@gmail.com>
2023-07-14 17:56:24 +0530

    add footer component to app component

--- src/frontend/src/App.jsx
+++ src/frontend/src/App.jsx
@@ -2,6 +2,7 @@ import Navbar from "./components/Navbar.jsx";
 import Hero from "./components/Hero.jsx";
 import About from "./components/About.jsx";
 import Projects from "./components/Projects.jsx";
+import Footer from "./components/Footer.jsx";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Hero />
       <About />
       <Projects />
+      <Footer />
     </div>
   );
 }
