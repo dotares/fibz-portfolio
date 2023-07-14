e86b7fb081602c8435db0a045733d3741ebbc410
fibbbonachos <arish2k3@gmail.com>
2023-07-14 17:41:22 +0530

    add main components in app component

--- src/frontend/src/App.jsx
+++ src/frontend/src/App.jsx
@@ -1,5 +1,17 @@
+import { Navbar } from "./components/Navbar.jsx";
+import { Hero } from "./components/Hero.jsx";
+import { About } from "./components/About.jsx";
+import { Projects } from "./components/Projects.jsx";
+
 function App() {
-  return <></>;
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+      <About />
+      <Projects />
+    </div>
+  );
 }
 
 export default App;
