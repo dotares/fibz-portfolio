73b4c73b69a5e715e1b5b4edff3f1f4b6d35c9cc
fibbbonachos <arish2k3@gmail.com>
2023-07-14 19:28:12 +0530

    update nav structure

--- src/frontend/src/components/Navbar.jsx
+++ src/frontend/src/components/Navbar.jsx
@@ -1,14 +1,18 @@
 export default function Navbar() {
   return (
     <div className="navbar-section">
-      <div className="home-link">
-        <a href="#">brand</a>
+      <div>
+        <a className="brand-logo" href="#">
+          brand
+        </a>
       </div>
-      <div className="about-link">
-        <a href="#about">about</a>
-      </div>
-      <div className="projects-link">
-        <a href="#projects">projects</a>
+      <div>
+        <a className="about-link" href="#about">
+          about
+        </a>
+        <a className="projects-link" href="#projects">
+          projects
+        </a>
       </div>
     </div>
   );
