cbddd8897e781b2dae5e83d30cdeeaefc809eddb
fibbbonachos <arish2k3@gmail.com>
2023-07-14 20:26:21 +0530

    add links in navbar

--- src/frontend/src/components/Navbar.jsx
+++ src/frontend/src/components/Navbar.jsx
@@ -1,19 +1,15 @@
 export default function Navbar() {
   return (
     <div className="navbar-section">
-      <div>
-        <a className="brand-logo" href="#">
-          brand
-        </a>
-      </div>
-      <div>
-        <a className="about-link" href="#about">
-          about
-        </a>
-        <a className="projects-link" href="#projects">
-          projects
-        </a>
-      </div>
+      <a className="brand-logo" href="#">
+        brand
+      </a>
+      <a className="about-link" href="#about">
+        about
+      </a>
+      <a className="projects-link" href="#projects">
+        projects
+      </a>
     </div>
   );
 }
