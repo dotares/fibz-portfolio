4a9650427d9dc1a46fbdbca7540f764b431e038b
fibbbonachos <arish2k3@gmail.com>
2023-07-15 10:55:07 +0530

    align new hero, no marquee

--- src/frontend/src/components/Hero.jsx
+++ src/frontend/src/components/Hero.jsx
@@ -1,19 +1,21 @@
 import heroImage from "../assets/Fibbonachos-HERO.png";
-import Marquee from "./Marquee";
 
 export default function Hero() {
   return (
     <>
-      <div className="hero-section p-8">
-        <h1 className="primary-header">
-          Hi, I'm Fibz aka Arish, and I'm a REAL full-stack developer.
-        </h1>
-        <h3 className="tertiary-header">
-          Let's cut the crap and build something great together
-        </h3>
-        <img src={heroImage} alt="Hero Image" />
+      <div className="hero-section flex items-center p-8">
+        <div>
+          <h1 className="primary-header">
+            Hi, I'm Fibz aka Arish, and I'm a REAL full-stack developer.
+          </h1>
+          <h3 className="tertiary-header">
+            Let's cut the crap and build something great together
+          </h3>
+        </div>
+        <div>
+          <img src={heroImage} alt="Hero Image" />
+        </div>
       </div>
-      <Marquee />
     </>
   );
 }
