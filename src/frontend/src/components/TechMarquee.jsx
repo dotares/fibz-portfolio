876a2d8dc8b90e956844588cc802a9e8fb9aa997
fibbbonachos <arish2k3@gmail.com>
2023-07-15 13:53:34 +0530

    add icons in techmarquee

--- src/frontend/src/components/TechMarquee.jsx
+++ src/frontend/src/components/TechMarquee.jsx
@@ -1,3 +1,33 @@
 export default function Marquee() {
-  return <div className="bg-zinc-800 my-12 py-12">Marquee</div>;
+  return (
+    <div className="bg-zinc-800 my-12 py-12 space-x-16 text-5xl">
+      <i class="devicon-c-plain"></i>
+      <i class="devicon-cplusplus-plain"></i>
+      <i class="devicon-javascript-plain"></i>
+      <i class="devicon-typescript-plain"></i>
+      <i class="devicon-python-plain"></i>
+      <i class="devicon-react-original"></i>
+      <i class="devicon-bootstrap-plain"></i>
+      <i class="devicon-css3-plain"></i>
+      <i class="devicon-html5-plain"></i>
+      <i class="devicon-sass-original"></i>
+      <i class="devicon-electron-original"></i>
+      <i class="devicon-webpack-plain"></i>
+      <i class="devicon-babel-plain"></i>
+      <i class="devicon-tailwindcss-original-wordmark"></i>
+      <i class="devicon-nodejs-plain"></i>
+      <i class="devicon-express-original"></i>
+      <i class="devicon-nginx-original"></i>
+      <i class="devicon-mongodb-plain"></i>
+      <i class="devicon-sqlite-plain"></i>
+      <i class="devicon-bash-plain"></i>
+      <i class="devicon-kubernetes-plain"></i>
+      <i class="devicon-docker-plain"></i>
+      <i class="devicon-figma-plain"></i>
+      <i class="devicon-nextjs-original"></i>
+      <i class="devicon-linux-plain"></i>
+      <i class="devicon-git-plain"></i>
+      <i class="devicon-github-original"></i>
+    </div>
+  );
 }
