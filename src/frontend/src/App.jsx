ad3216d1efe1f5ebab0797a52b1e8309e7f4906d
fibbbonachos <arish2k3@gmail.com>
2023-07-15 12:04:06 +0530

    add marquee section

--- src/frontend/src/App.jsx
+++ src/frontend/src/App.jsx
@@ -3,12 +3,14 @@ import Hero from "./components/Hero.jsx";
 import About from "./components/About.jsx";
 import Projects from "./components/Projects.jsx";
 import Footer from "./components/Footer.jsx";
+import Marquee from "./components/TechMarquee.jsx";
 
 function App() {
   return (
     <div className="main-container">
       <Navbar />
       <Hero />
+      <Marquee />
       <About />
       <Projects />
       <Footer />
---
+++ src/frontend/src/components/TechMarquee.jsx
@@ -0,0 +1,3 @@
+export default function Marquee() {
+  return <div className="bg-zinc-800 my-12 py-12">Marquee</div>;
+}
