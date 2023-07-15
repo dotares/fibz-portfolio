5de0d10a52791610425f33e38051393ce7ec7009
fibbbonachos <arish2k3@gmail.com>
2023-07-15 09:55:07 +0530

    add marquee jsx but not configured it yet

--- src/frontend/src/components/Hero.jsx
+++ src/frontend/src/components/Hero.jsx
@@ -3,6 +3,22 @@ import heroImage from "../assets/Fibbonachos-HERO.png";
 export default function Hero() {
   return (
     <div className="hero-section">
+      <div className="relative marquee-section flex overflow-x-hidden">
+        <div className="animate-marquee whitespace-nowrap">
+          <span>Fibbonachos</span>
+          <span>Fibbonachos</span>
+          <span>Fibbonachos</span>
+          <span>Fibbonachos</span>
+          <span>Fibbonachos</span>
+        </div>
+        <div className="absolute animate-marquee2 whitespace-nowrap">
+          <span>Fibbonachos</span>
+          <span>Fibbonachos</span>
+          <span>Fibbonachos</span>
+          <span>Fibbonachos</span>
+          <span>Fibbonachos</span>
+        </div>
+      </div>
       <h1 className="primary-header">
         Hi, I'm Fibz aka Arish, and I'm a REAL full-stack developer.
       </h1>
