b9b03a4d9841b8003884d344876c15e826ec921d
fibbbonachos <arish2k3@gmail.com>
2023-07-15 09:36:55 +0530

    added a small space in footer text

--- src/frontend/src/components/Footer.jsx
+++ src/frontend/src/components/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <div className="footer-section">
-      <p className="paragraph-content">©2023 Fibbonachos</p>
+      <p className="paragraph-content">© 2023 Fibbonachos</p>
     </div>
   );
 }
