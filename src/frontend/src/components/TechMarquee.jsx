f84559163f315de09b194d8958fcec5163199bcd
fibbbonachos <arish2k3@gmail.com>
2023-07-16 11:31:42 +0530

    change from carousel to stacked layout marquee section

--- src/frontend/src/components/TechMarquee.jsx
+++ src/frontend/src/components/TechMarquee.jsx
@@ -1,7 +1,7 @@
 export default function Marquee() {
     return (
-        <div className="bg-zinc-800 flex mt-12 py-14 text-5xl text-orange-100 overflow-hidden">
-            <div className="animate-marquee whitespace-nowrap space-x-16">
+        <div className="bg-zinc-800 flex flex-col mt-12 py-14 text-5xl space-y-5 text-orange-100 place-items-center overflow-hidden">
+            <div className="whitespace-nowrap space-x-14">
                 <i class="devicon-tailwindcss-original-wordmark"></i>
                 <i class="devicon-nodejs-plain"></i>
                 <i class="devicon-express-original"></i>
@@ -12,12 +12,8 @@ export default function Marquee() {
                 <i class="devicon-kubernetes-plain"></i>
                 <i class="devicon-docker-plain"></i>
                 <i class="devicon-figma-plain"></i>
-                <i class="devicon-nextjs-original"></i>
-                <i class="devicon-linux-plain"></i>
-                <i class="devicon-git-plain"></i>
-                <i class="devicon-github-original"></i>
             </div>
-            <div className="animate-marquee2 top-0 whitespace-nowrap space-x-16">
+            <div className="whitespace-nowrap space-x-14">
                 <i class="devicon-c-plain"></i>
                 <i class="devicon-cplusplus-plain"></i>
                 <i class="devicon-javascript-plain"></i>
@@ -27,10 +23,16 @@ export default function Marquee() {
                 <i class="devicon-bootstrap-plain"></i>
                 <i class="devicon-css3-plain"></i>
                 <i class="devicon-html5-plain"></i>
-                <i class="devicon-sass-original"></i>
+            </div>
+            <div className="whitespace-nowrap space-x-14">
+                <i class="devicon-nextjs-original"></i>
+                <i class="devicon-linux-plain"></i>
+                <i class="devicon-git-plain"></i>
+                <i class="devicon-github-original"></i>
                 <i class="devicon-electron-original"></i>
                 <i class="devicon-webpack-plain"></i>
                 <i class="devicon-babel-plain"></i>
+                <i class="devicon-sass-original"></i>
             </div>
         </div>
     );
