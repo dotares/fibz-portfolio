6dd9fd9ca19589ca970c23b1dc9192c8eaf8aaf0
fibbbonachos <arish2k3@gmail.com>
2023-07-16 14:13:03 +0530

    make hero section responsive

--- src/frontend/src/components/Hero.jsx
+++ src/frontend/src/components/Hero.jsx
@@ -2,24 +2,22 @@ import heroImage from "../assets/Fibbonachos-HERO.png";
 
 export default function Hero() {
     return (
-        <>
-            <div className="hero-section flex flex-col place-items-center">
-                <div>
-                    <img
-                        src={heroImage}
-                        alt="Hero Image"
-                        className="rounded-full bg-orange-600 shadow-xl mb-8 shadow-zinc-950 border-orange-600 border-2 h-52"
-                    />
-                </div>
-                <h1 className="primary-header text-5xl font-mono mb-4">
-                    Hi, I'm{" "}
-                    <span className="text-orange-600 font-bold">Arish </span>aka
-                    Fibz👋
-                </h1>
-                <h3 className="secondary-header font-mono text-2xl">
-                    Building digital products and experiences.
-                </h3>
+        <div className="hero-section sm:px-0 px-5 flex flex-col place-items-center">
+            <div>
+                <img
+                    src={heroImage}
+                    alt="Hero Image"
+                    className="rounded-full bg-orange-600 shadow-xl mb-8 shadow-zinc-950 border-orange-600 border-2 h-40 sm:h-52"
+                />
             </div>
-        </>
+            <h1 className="primary-header text-center text-2xl sm:text-5xl font-mono mb-4">
+                Hi, I'm{" "}
+                <span className="text-orange-600 font-bold">Arish </span>aka
+                Fibz👋
+            </h1>
+            <h3 className="secondary-header font-mono text-center text-lg sm:text-2xl">
+                Building digital products and experiences.
+            </h3>
+        </div>
     );
 }
