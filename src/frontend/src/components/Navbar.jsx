b4028b5e05a9662a18d1cf8511ca60a53292b8b2
fibbbonachos <arish2k3@gmail.com>
2023-07-16 14:08:50 +0530

    make navbar responsive

--- src/frontend/src/components/Navbar.jsx
+++ src/frontend/src/components/Navbar.jsx
@@ -1,15 +1,15 @@
 export default function Navbar() {
     return (
-        <div className="navbar-section text-xl font-mono flex px-32 py-12 place-content-between sticky top-0 bg-zinc-900 drop-shadow-xl">
+        <div className="navbar-section font-mono flex sm:px-32 sm:py-6 px-6 py-4 place-content-between items-center sticky top-0 bg-zinc-900">
             {" "}
             <div>
                 <a className="brand-logo" href="#">
-                    <span className="text-orange-600 text-2xl font-bold">
+                    <span className="text-orange-600 text-lg sm:text-2xl font-bold">
                         Fibbonachos.
                     </span>
                 </a>
             </div>
-            <div className="space-x-8">
+            <div className="space-x-4 text-md sm:text-xl sm:space-x-8">
                 <a className="about-link" href="#about">
                     About
                 </a>
--- src/frontend/src/components/TechMarquee.jsx
+++ src/frontend/src/components/TechMarquee.jsx
@@ -1,38 +1,38 @@
 export default function Marquee() {
     return (
-        <div className="bg-zinc-800 flex flex-col mt-12 py-14 text-5xl space-y-5 text-orange-100 place-items-center overflow-hidden">
+        <div className="bg-zinc-800 flex flex-col mt-12 py-14 text-5xl space-y-5 text-orange-100 place-items-center items-center overflow-hidden">
             <div className="whitespace-nowrap space-x-14">
-                <i class="devicon-tailwindcss-original-wordmark"></i>
-                <i class="devicon-nodejs-plain"></i>
-                <i class="devicon-express-original"></i>
-                <i class="devicon-nginx-original"></i>
-                <i class="devicon-mongodb-plain"></i>
-                <i class="devicon-sqlite-plain"></i>
-                <i class="devicon-bash-plain"></i>
-                <i class="devicon-kubernetes-plain"></i>
-                <i class="devicon-docker-plain"></i>
-                <i class="devicon-figma-plain"></i>
+                <i className="devicon-tailwindcss-original-wordmark"></i>
+                <i className="devicon-nodejs-plain"></i>
+                <i className="devicon-express-original"></i>
+                <i className="devicon-nginx-original"></i>
+                <i className="devicon-mongodb-plain"></i>
+                <i className="devicon-sqlite-plain"></i>
+                <i className="devicon-bash-plain"></i>
+                <i className="devicon-kubernetes-plain"></i>
+                <i className="devicon-docker-plain"></i>
+                <i className="devicon-figma-plain"></i>
             </div>
             <div className="whitespace-nowrap space-x-14">
-                <i class="devicon-c-plain"></i>
-                <i class="devicon-cplusplus-plain"></i>
-                <i class="devicon-javascript-plain"></i>
-                <i class="devicon-typescript-plain"></i>
-                <i class="devicon-python-plain"></i>
-                <i class="devicon-react-original"></i>
-                <i class="devicon-bootstrap-plain"></i>
-                <i class="devicon-css3-plain"></i>
-                <i class="devicon-html5-plain"></i>
+                <i className="devicon-c-plain"></i>
+                <i className="devicon-cplusplus-plain"></i>
+                <i className="devicon-javascript-plain"></i>
+                <i className="devicon-typescript-plain"></i>
+                <i className="devicon-python-plain"></i>
+                <i className="devicon-react-original"></i>
+                <i className="devicon-bootstrap-plain"></i>
+                <i className="devicon-css3-plain"></i>
+                <i className="devicon-html5-plain"></i>
             </div>
             <div className="whitespace-nowrap space-x-14">
-                <i class="devicon-nextjs-original"></i>
-                <i class="devicon-linux-plain"></i>
-                <i class="devicon-git-plain"></i>
-                <i class="devicon-github-original"></i>
-                <i class="devicon-electron-original"></i>
-                <i class="devicon-webpack-plain"></i>
-                <i class="devicon-babel-plain"></i>
-                <i class="devicon-sass-original"></i>
+                <i className="devicon-nextjs-original"></i>
+                <i className="devicon-linux-plain"></i>
+                <i className="devicon-git-plain"></i>
+                <i className="devicon-github-original"></i>
+                <i className="devicon-electron-original"></i>
+                <i className="devicon-webpack-plain"></i>
+                <i className="devicon-babel-plain"></i>
+                <i className="devicon-sass-original"></i>
             </div>
         </div>
     );
