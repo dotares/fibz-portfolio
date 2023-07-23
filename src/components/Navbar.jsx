a97e09b590e79b72ba6d9fe2a69b8794b9890825
fibbbonachos <arish2k3@gmail.com>
2023-07-23 13:50:50 +0530

    add blur on navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,19 @@
 export default function Navbar() {
   return (
-    <div className="navbar-section font-mono flex sm:px-32 sm:py-6 px-6 py-4 place-content-between items-center sticky top-0 bg-zinc-100">
+    <div className="navbar-section font-mono flex sm:px-32 sm:py-6 px-6 py-4 place-content-between items-center sticky top-0 backdrop-blur-lg bg-black mix-blend-difference">
       {" "}
       <div>
         <a className="brand-logo" href="#">
-          <span className="text-zinc-900 text-lg sm:text-2xl font-bold">
+          <span className="text-zinc-100 text-lg sm:text-2xl font-bold">
             Fibbonachos.
           </span>
         </a>
       </div>
       <div className="space-x-4 text-md sm:text-xl sm:space-x-8">
-        <a className="about-link" href="#about">
+        <a className="about-link text-zinc-100" href="#about">
           About
         </a>
-        <a className="projects-link" href="#projects">
+        <a className="projects-link text-zinc-100" href="#projects">
           Projects
         </a>
       </div>
