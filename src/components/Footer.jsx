3311114821b8db2fe49e65aa4767e4fb0b777c73
fibbbonachos <arish2k3@gmail.com>
2023-07-26 23:42:58 +0530

    change backgrounds of projects and footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
-    <div className="footer-section p-2">
-      <p className="paragraph-content text-center text-black font-montserrat">
+    <div className="footer-section p-2 bg-zinc-800">
+      <p className="paragraph-content text-center text-zinc-200 font-montserrat">
         © 2023 Fibbonachos
       </p>
     </div>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,11 +1,14 @@
 export default function Projects() {
   return (
-    <div id="projects" className="projects-section px-8 sm:px-32 py-4 sm:py-12">
-      <h2 className="secondary-header text-xl sm:text-2xl font-jetbrainsmono">
+    <div
+      id="projects"
+      className="projects-section bg-zinc-800 px-8 sm:px-32 py-4 sm:py-12"
+    >
+      <h2 className="secondary-header text-white text-xl sm:text-2xl font-jetbrainsmono">
         {" "}
         My Projects 🛠️
       </h2>
-      <p className="paragraph-content font-montserrat text-zinc-500">
+      <p className="paragraph-content font-montserrat text-zinc-200">
         WORK IN PROGRESS!
       </p>
     </div>
