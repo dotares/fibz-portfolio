0533ab5f71dc5d48aed205c10fa1df7de570d08c
Fibz <arish2k3@gmail.com>
2023-07-31 11:21:29 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 export default function Navbar() {
   return (
-    <div className="navbar-section border-b-2 border-zinc-100 font-jetbrainsmono flex sm:px-32 sm:py-6 px-6 py-4 place-content-between items-center sticky top-0 backdrop-blur-xl bg-black mix-blend-difference">
+    <div className="navbar-section font-jetbrainsmono flex sm:px-32 sm:py-6 px-6 py-4 place-content-between items-center sticky top-0 backdrop-blur-xl bg-black mix-blend-difference">
       {" "}
       <div>
         <a className="brand-logo" href="#">
